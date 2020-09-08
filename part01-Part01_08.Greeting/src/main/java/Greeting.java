fca32c3bf953f73e0c7fd6d7be508f2d9d9dc9af
Ray Wu <raywupharmd@outlook.com>
2020-09-08 14:11:37 -0400

    added solutions for 1.1 to 1.16

---
+++ part01-Part01_08.Greeting/src/main/java/Greeting.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What's your name?");
+        // Write your program here
+        String message = scanner.nextLine();
+        System.out.println("Hi " + message);
+    }
+}
---
+++ part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class SecondsInADay {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+        System.out.println("How many days would you like to convert to seconds?");
+        int days = Integer.valueOf(scanner.nextLine());
+        int secondsInDay = 86400;
+        System.out.println(days * secondsInDay);
+    }
+}
