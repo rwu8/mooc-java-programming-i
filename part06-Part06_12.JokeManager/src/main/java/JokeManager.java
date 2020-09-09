e0989c78c66b45b1459a082d3ee06f50fa3f6574
Ray Wu <raywupharmd@outlook.com>
2020-09-09 18:46:33 -0400

    added solution for 06-12

--- part06-Part06_12.JokeManager/src/main/java/JokeManager.java
+++ part06-Part06_12.JokeManager/src/main/java/JokeManager.java
@@ -1,17 +1,31 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class JokeManager {
-	private ArrayList<String> jokes;
+    private ArrayList<String> jokes;
 
-	public JokeManager() {
-		this.jokes = new ArrayList<>();
-	}
+    public JokeManager() {
+        this.jokes = new ArrayList<>();
+    }
 
-	public void addJoke(String joke) {
-		this.jokes.add(joke);
-	}
+    public void addJoke(String joke) {
+        this.jokes.add(joke);
+    }
 
-	public String drawJoke() {
+    public String drawJoke() {
+        if (jokes.isEmpty()) {
+            return "Jokes are in short supply.";
+        }
 
-	}
+        Random draw = new Random();
+        int randomJoke = draw.nextInt(jokes.size());
+
+        return this.jokes.get(randomJoke);
+    }
+
+    public void printJokes() {
+        for (String joke : jokes) {
+            System.out.println(joke);
+        }
+    }
 }
