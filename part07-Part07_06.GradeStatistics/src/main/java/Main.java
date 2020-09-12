420a8ce1127b5947a6762f3fb95bdbc768f22eca
rwu <M@rw-mba.local>
2020-09-11 22:05:12 -0400

    completed all 7 parts of Java I

--- part07-Part07_06.GradeStatistics/src/main/java/Main.java
+++ part07-Part07_06.GradeStatistics/src/main/java/Main.java
@@ -5,8 +5,28 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // Write your program here -- consider breaking the program into 
+		Points pointList = new Points();
+        // Write your program here -- consider breaking the program into
         // multiple classes.
+		System.out.println("Enter point totals, -1 stops:");
+		while (true) {
+			Integer input = Integer.valueOf(scanner.nextLine());
+			if (input == -1) {
+				break;
+			}
+
+			pointList.add(input);
+		}
+		double average = pointList.average();
+		System.out.println("Point average (all): " + average);
+		double passingAverage = pointList.passingAverage();
+		if (passingAverage == 0.0) {
+			System.out.println("Point average (passing): -");
+		} else {
+			System.out.println("Point average (passing): " + passingAverage);
+		}
+		System.out.println("Pass percentage: " + pointList.passPercentage());
+		System.out.println("Grade distribution:");
+		pointList.gradeDistribution();
     }
 }
